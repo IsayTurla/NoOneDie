044b390a81ed41855c992680e87cfc4609e8c84f
Chao Zhong <zhongchao.ustc@gmail.com>
2014-10-29 02:32:35 +0800

    cm

--- js/gads.js
+++ js/gads.js
@@ -12,16 +12,23 @@ var is_mobile = ipad || ipod || iphone || android || wp || tablet || touch || mo
 function show_ad() {
     if (true) {
         var big_ad = document.body.clientWidth > 468;
+        var gadsDiv = document.getElementById("gads");
+        gadsDiv.style.width = "100%";
+        // gadsDiv.width = document.body.clientWidth;
+        // gadsDiv.left = 0;
+        // gadsDiv.bottom = 0;
         if (is_mobile && !big_ad) {
             window.google_ad_client = "ca-pub-1667278402120283";
             window.google_ad_slot = "4046453012";
             window.google_ad_width = 320;// * (document.body.clientWidth * 1.0 / 320.0);
             window.google_ad_height = 50;// * (document.body.clientWidth * 1.0 / 320.0);
+            gadsDiv.style.height = "50px";
         } else {
             window.google_ad_client = "ca-pub-1667278402120283";
             window.google_ad_slot = "8057850217";
             window.google_ad_width = 728;// * (document.body.clientWidth * 1.0 / 320.0);
             window.google_ad_height = 90;// * (document.body.clientWidth * 1.0 / 320.0);
+            gadsDiv.style.height = 90 + "px";
         }
         document.write("<script src='http://pagead2.googlesyndication.com/pagead/show_ads.js'><\/script><br/>");
     }
